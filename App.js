8e162264479906784b111b9203c0779e111109d5
millena.gomes <millena.gomes@ppm-jamf-28643.bcocorporate.ad>
2023-11-14 12:34:09 -0300

    readme

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
+import React, { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Keyboard } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const ageInput = useRef();
+  const nextStep = () => {
+    Keyboard.dismiss();
+    console.log(name);
+    console.log(age);
+    setName('');
+    setAge('');
+  }
+
+  function HomeScreen({ navigation: { navigate } }) {
+    return (
+      <View>
+        <Text>This is the home screen of the app</Text>
+        <Button
+          onPress={() =>
+            navigate('Profile', { names: ['Brent', 'Satya', 'Michaś'] })
+          }
+          title="Go to Brent's profile"
+        />
+      </View>
+    );
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        placeholder="Digite o seu nome"
+        defaultValue={name}
+        onChangeText={newText => setName(newText)}
+        style={{marginBottom: 20}}
+        returnKeyType="next"
+        onSubmitEditing={() => ageInput.current.focus()}
+      />
+      <TextInput
+        ref={ageInput}
+        placeholder="Digite sua idade"
+        keyboardType="numeric"
+        defaultValue={age}
+        onChangeText={newText => setAge(newText)}
+        style={{marginBottom: 20}}
+      />
+      <Button onPress={nextStep} title="Continuar"/>
       <StatusBar style="auto" />
+  
+
+    <View>
+     
+
+    </View>
     </View>
   );
 }
